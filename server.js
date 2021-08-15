6ae65853be003bc32bb6ed1a64c32ccd417879c3
niwa-niwa <urban.hang274@gmail.com>
2021-08-15 18:23:11 +0900

    implemented POST request

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 var http = require("http");
 var fs = require('fs');
 var ejs = require('ejs');
+var qs = require('querystring');
 var settings = require('./settings');
 
 var server = http.createServer();
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-var n = 0;
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+var posts = [];
 
-server.on("request", function (req, res) {
-  n++;
+function renderForm(posts, res){
   var data = ejs.render(template, {
-    title: "hello",
-    content: "<strong>World!</strong>",
-    n:n
-  })
+    posts: posts
+  });
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(data);
   res.end();
+}
+
+server.on("request", function (req, res) {
+  if(req.method === 'POST'){
+    req.data = "";
+    req.on("data", function(chunk){
+      req.data += chunk;
+    });
+    req.on('end', function(){
+      var query = qs.parse(req.data);
+      posts.push(query.name);
+      renderForm(posts, res);
+    });
+  }else{
+    renderForm(posts, res);
+  }
 });
 
 server.listen(settings.port, settings.host);
