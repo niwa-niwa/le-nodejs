7d502c4f89b56a928b4c76ad498f27891581c58c
niwa-niwa <urban.hang274@gmail.com>
2021-08-15 17:50:10 +0900

    implemented ejs

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var http = require("http");
 var fs = require('fs');
+var ejs = require('ejs');
 var settings = require('./settings');
 
 var server = http.createServer();
-
-var msg;
+var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+var n = 0;
 
 server.on("request", function (req, res) {
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
-    if(err){
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.writeHead("Not Found!");
-      return res.end();
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+  n++;
+  var data = ejs.render(template, {
+    title: "hello",
+    content: "<strong>World!</strong>",
+    n:n
+  })
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(data);
+  res.end();
 });
 
 server.listen(settings.port, settings.host);
