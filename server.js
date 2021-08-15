3f6a381d2c0b265fa3706c5b9f9dcfe1b7c47fbf
niwa-niwa <urban.hang274@gmail.com>
2021-08-15 17:06:17 +0900

    made server

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+var settings = require('./settings');
+
+var server = http.createServer();
+server.on("request", function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.write("hello world !");
+  res.end();
+});
+server.listen(settings.port, settings.host);
+
+console.log(`server listening ... http://${settings.host}:${settings.port}`);
